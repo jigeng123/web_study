4c278b8a4dc1a1d819737fbb60399b594be89e83
JIGENG <houjiageng000@gmail.com>
2025-09-24 13:38:21 +0800

    body添加了边框

--- 专注模式.js
+++ 专注模式.js
@@ -9,6 +9,9 @@
   // 清空 body
   document.body.innerHTML = "";
 
+  // 设置 body 内边距
+  document.body.style.padding = "100px 80px 0px 80px";
+
   // 将目标元素添加回 body
   document.body.appendChild(target);
 })("你的选择器");
