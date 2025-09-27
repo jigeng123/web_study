04510925ecb04540755203a03d519b022545d7c0
JIGENG_YIhome <houjiageng000@gmail.com>
2025-09-28 02:54:28 +0800

    学习React+vite

---
+++ vite/firstViteProject/src/main.tsx
@@ -0,0 +1,12 @@
+//React模块导入
+//导入css和tsx核心脚本
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
